b1522008bbe7e5edc1fff191b52e31df416ade1d
FrancescoAmato87 <francesco.duff@gmail.com>
2020-10-27 17:34:16 +0100

    Ultima modifica

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
 
     // ............H A M B U R G H E R..............
 
-var hamburgerMenu = $('.hamburger-menu');
+// var hamburgerMenu = $('.hamburger-menu');
+//
+// hamburgerMenu.click( function () {
+// $('.hamburgher-menu').slideDown('slow');
+// });
+//
+// // solo i figli della lista
+// $('.header-right').click( function() {
+//   $('.hamburger-menu').slideToggle('slow');
+// });
 
-hamburgerMenu.click( function () {
-$('.hamburgher-menu').slideDown('slow');
+
+
+var hamburgerIcon = $('.header-right > a ')
+// console.log(hamburgerIcon);
+
+var menu = $('.hamburger-menu');
+var closeIcon = $('.hamburger-menu .close');
+
+// click
+hamburgerIcon.click( function() {
+  menu.addClass('active');
 });
 
-// solo i figli della lista
-$('.header-right').click( function() {
-  $('.hamburger-menu').slideToggle('slow');
+closeIcon.click(function () {
+  menu.removeClass('active');
 });
